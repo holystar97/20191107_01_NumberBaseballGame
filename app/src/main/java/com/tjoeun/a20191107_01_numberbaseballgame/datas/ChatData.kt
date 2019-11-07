d4c0829e0ebaed8c65ed89ab9a0efedbe86a5f30
holystar97 <mkrice500@gmail.com>
2019-11-07 20:15:11 +0900

    데이터 클래스 생성

---
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/datas/ChatData.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191107_01_numberbaseballgame.datas
+
+import java.io.Serializable
+
+class ChatData(message:String,speaker:String):Serializable{
+
+    var message=message
+    var speaker=speaker
+    
+
+
+}
